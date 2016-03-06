db2b7b767817c1a3798721ae9b74cd6379cd9184
Morten Kals <m.kals@me.com>
2016-03-06 13:43:07 +0100

    Initial Commit

---
+++ MKNeuralNet/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MKNeuralNet
+//
+//  Created by Morten Kals on 06/03/16.
+//  Copyright © 2016 Morten Kals. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
